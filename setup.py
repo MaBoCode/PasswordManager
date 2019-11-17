ec8ba0c22b82dac96b861b4fe3063074b4ffcd1b
Matthias Brown <matthias.brown@etu.univ-tours.fr>
2019-11-17 02:46:32 +0100

    Fill requirements and setup

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup(
     name="PasswordManager",
     version="2.0",
     packages=find_packages()
+	
+	install_requires=['cryptography==2.6.1', 'tabulate==0.8.5']
+
+	# Metadata
+	author="Matthias Brown"
+	author_email="matthias.brown@etu.univ-tours.fr"
+	description="Best command line Password Manager!"
+	keywords="password manager"
 )
